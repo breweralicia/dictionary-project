13a86accc43747f93bcfbb40bd12af7252b547f7
breweralicia <aliciapadgett@gmail.com>
2021-07-19 16:48:46 -0500

    photo update

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,32 @@
 import React, { useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 import "./Dictionary.css";
 
 export default function Dictionary(){
   let [keyword, setKeywor] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0])
     //console.log(response.data[0].meanings[0].definitions[0].definition);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search(event){
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
     axios.get(apiUrl).then(handleResponse);
-    
+
+    let pexelsApiKey = `563492ad6f91700001000001e4fa46e233be46e68562e79c899b6b95`  
+    let pexelsApiUrl = `https://api.pexels.com/v1//search?query=${keyword}&per_page=1`  
+    axios.get(pexelsApiUrl, { headers: {"Authorization" : `Bearer ${pexelsApiKey}`}}).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -37,6 +46,7 @@ export default function Dictionary(){
       </div>
       </section>
       <Results results={results}/>
+      <Photos photos={photos}/>
     </div>
   )
   
