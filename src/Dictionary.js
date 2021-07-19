871ac22af718f24694aa6d4521a05ab74d7643ed
breweralicia <aliciapadgett@gmail.com>
2021-07-18 22:42:59 -0500

    search api update

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,26 @@
 import React, { useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary(){
   let [keyword, setKeywor] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event){
     event.preventDefault();
-    alert(`searching for ${keyword} definition`)
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
+    axios.get(apiUrl).then(handleResponse);
+    
   }
 
   function handleKeywordChange(event) {
     //console.log(event.target.value)
     setKeywor(event.target.value);
   }
+
 
   return (
     <div className="Dictionary">
